fa71a0b084e44023e7a643525a68c0e208bc2c9e
sharknetbg <32771426+sharknetbg@users.noreply.github.com>
2023-05-28 01:04:50 +0300

    main

--- Karrots.sol
+++ Karrots.sol
@@ -1,35 +1,6 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-/**
-
-             \     /
-             \\   //
-              )\-/(
-              /e e\
-             ( =Y= )
-             /`-!-'\
-        ____/ /___\ \
-   \   /    ```    ```~~"--.,_
-`-._\ /                       `~~"--.,_
------>|                                `~~"--.,_
-_.-'/ \                                         ~~"--.,_
-   /   \_________________________,,,,....----""""~~~~````
-
-
-     88     888    d8P         d8888 8888888b.  8888888b.   .d88888b. 88888888888 
- .d88888b.  888   d8P         d88888 888   Y88b 888   Y88b d88P" "Y88b    888     
-d88P 88"88b 888  d8P         d88P888 888    888 888    888 888     888    888     
-Y88b.88     888d88K         d88P 888 888   d88P 888   d88P 888     888    888     
- "Y88888b.  8888888b       d88P  888 8888888P"  8888888P"  888     888    888     
-     88"88b 888  Y88b     d88P   888 888 T88b   888 T88b   888     888    888     
-Y88b 88.88P 888   Y88b   d8888888888 888  T88b  888  T88b  Y88b. .d88P    888     
- "Y88888P"  888    Y88b d88P     888 888   T88b 888   T88b  "Y88888P"     888     
-     88                                                                           
-                                                                                  
-    https://twitter.com/Karrot_gg                                                                    
- */
-
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
